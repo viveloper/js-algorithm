ef22367f6bf27f4d9b639f0048cd016257c9faa7
viveloper <ucsik1234@gmail.com>
2021-01-01 18:12:43 +0900

    added graph-traversal

--- DataStructure/Graph.js
+++ DataStructure/Graph.js
@@ -23,6 +23,46 @@ class Graph {
     });
     delete this.adjacencyList[v];
   }
+
+  dfs(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+    visited[start] = true;
+    helper(start);
+    return result;
+
+    function helper(vertex) {
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          helper(neighbor);
+        }
+      });
+    }
+  }
+
+  bfs(start) {
+    const result = [];
+    const visited = {};
+    const queue = [];
+    queue.push(start);
+    visited[start] = true;
+
+    while (queue.length > 0) {
+      const vertex = queue.shift();
+      result.push(vertex);
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
 }
 
 const graph = new Graph();
@@ -32,5 +72,13 @@ graph.addVertex('yongin');
 graph.addEdge('seoul', 'yongin');
 graph.addVertex('osan');
 graph.addEdge('yongin', 'osan');
-graph.removeVertex('yongin');
+graph.addVertex('ansung');
+graph.addEdge('yongin', 'ansung');
+graph.addVertex('pyungtaek');
+graph.addEdge('pyungtaek', 'ansung');
+graph.addEdge('pyungtaek', 'seoul');
+graph.addEdge('osan', 'seoul');
+
 console.log(graph.adjacencyList);
+console.log(graph.dfs('seoul'));
+console.log(graph.bfs('seoul'));
